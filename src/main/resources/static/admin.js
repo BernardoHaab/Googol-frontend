9294f86217d608d80ad934d30377e35b5f7e9a86
Bernardo Luiz Haab <bernardohaab@gmail.com>
2024-05-12 21:31:14 +0100

    feat: Show list of barrels and top search

--- src/main/resources/static/admin.js
+++ src/main/resources/static/admin.js
@@ -4,14 +4,12 @@ function connect() {
   var socket = new SockJS('/websocket');
   stompClient = Stomp.over(socket);
   stompClient.connect({}, function (frame) {
-    // setConnected(true);
-    console.log('Connected: ' + frame);
     stompClient.subscribe('/admin/updates', function (message) {
-      console.log('Recebeu');
-      console.log('Message: ', message);
-
       try {
-        console.log(JSON.parse(message.body));
+        const resJson = JSON.parse(message.body);
+
+        showBarrels(resJson.barrels);
+        updateTopSearchList(resJson.topSearch);
       } catch (error) {
         console.log('Error');
         console.log(error);
@@ -34,6 +32,33 @@ window.addEventListener('beforeunload', function (e) {
   // e.returnValue = '';
   disconnect();
 });
+
+function showBarrels(barrels) {
+  $('#barrels').html('');
+
+  barrels.forEach((barrel) => {
+    $('#barrels').append(`
+      <tr >
+        <td><span> ${barrel.hostAddress} </span></td>
+        <td><span> ${barrel.isActive ? 'Ativo' : 'Inativo'} </span></td>
+        <td><span> ${barrel.avgResponseTime} (decimos de segundos) </span></td>
+      </tr>
+      `);
+  });
+}
+
+function updateTopSearchList(listTopSearch) {
+  $('#listTopSearch').html('');
+
+  listTopSearch.forEach((topSearch) => {
+    $('#listTopSearch').append(`
+      <tr >
+        <td><span> ${topSearch.search} </span></td>
+        <td><span> ${topSearch.count} </span></td>
+      </tr>
+      `);
+  });
+}
 
 function sendMessage() {
   stompClient.send(
